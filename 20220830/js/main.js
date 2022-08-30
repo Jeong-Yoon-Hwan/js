7763e45c06361bca99ab7a19f20c747c0d61c2ad
unknown <Jeong-Yoon-Hwan>
2022-08-31 02:43:05 +0900

    태그추가

--- 20220830/js/main.js
+++ 20220830/js/main.js
@@ -1,15 +1,19 @@
-import makeTag from "./makeTag.js";
+import createElement from "./makeTag.js";
 import { elementStyling, flexSet, mainStyle, rootStyle} from "./style.js";
 
 
-const root = document.getElementById('root'); //* root 선언
-makeTag(root,'main',''); //* main 생성
+const root = document.getElementById('root');
 
-const main = document.querySelector("#root > main"); //* main 선언
-//*article 생성
-for(let i=0;i<3;i++){
-  makeTag(main,'article','');
-}
+root.innerHTML =`
+  ${createElement("header")}
+  ${createElement("main")}
+  ${createElement("footer")}
+`;
+
+const main = document.querySelector("#root > main");
+
+
+const footer = document.querySelector("#root > footer");
 
 elementStyling(root,rootStyle); //* root 스타일링
 flexSet(root,'row','center','center');
--- 20220830/js/makeTag.js
+++ 20220830/js/makeTag.js
@@ -1,7 +1,14 @@
-const makeTag = (parent,tagName,html) =>{
-  const tag = document.createElement(tagName);
-  tag.innerHTML = html;
-  parent.appendChild(tag);
+// export const makeTag = (parent,tagName,html) =>{
+//   const tag = document.createElement(tagName);
+//   tag.innerHTML = html;
+//   parent.appendChild(tag);
+// }
+
+const createElement = (tagName)=>{
+  if(typeof(tagName) === "string"){
+    return `<${tagName}></${tagName}>`;
+  }
 }
 
-export default makeTag;
+export default createElement;
+
