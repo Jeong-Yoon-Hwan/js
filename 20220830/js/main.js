5b8a1fbb23ed9474f3b19e3ed884d1962f2ddd89
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-08-30 17:33:07 +0900

    style 추가

--- 20220830/js/main.js
+++ 20220830/js/main.js
@@ -1,5 +1,26 @@
 import makeTag from "./makeTag.js";
+import { elementStyling, flexSet, mainStyle, rootStyle} from "./style.js";
 
 //root 선언
 const root = document.getElementById('root');
 makeTag(root,'main','');
+
+const main = document.querySelector("#root > main");
+
+console.dir(root);
+
+
+elementStyling(root,rootStyle);
+elementStyling(main,mainStyle); 
+//  elementStyling(root,{
+//   width:"100vw",
+//   height:"100vh",
+//   backgroundColor:"beige"
+//  },"");
+
+//  elementStyling(main,{
+//   width:"100px",
+//   height:"100px",
+//   backgroundColor:"black"
+//  },"");
+
--- 20220830/js/style.js
+++ 20220830/js/style.js
@@ -1,17 +1,25 @@
-export function elementStyling(element, styleProps, textContent = ""){
+export function elementStyling(element, styleProps){
   for(const key in styleProps){
     element.style[key] = styleProps[key];
   }
-  element.textContent = textContent;
+}
+
+export function flexSet(element,direction,justify,align){
+  element.style.display="flex";
+  element.style.direction=direction;
+  element.style.justifyContent=justify;
+  element.style.alignItems=align;
 }
 
 
-const mainStyle = {
-  backgroundColor : "#333",
-  width : "100vw",
-  height : "80vh"
+export const rootStyle= {
+  width:"100vw",
+  height:"100vh",
+  backgroundColor:"beige"
 }
-
-const rootStyle = {
-  backgroundColor : "beige"
+export const mainStyle = {
+  width: "100vw",
+  height : "80vh",
+  backgroundColor:"black"
 }
+//elementStyling(main,mainStyle,"");
